789e5d948b53d2ab0059a83034f6952e8a1b5b64
yinglong.zhang <yinglong.zhang@funplus.com>
2017-08-05 15:40:40 +0800

    surport carthage
    surport carthage

---
+++ NetDiagFramework/NetDiagFramework.h
@@ -0,0 +1,19 @@
+//
+//  NetDiagFramework.h
+//  NetDiagFramework
+//
+//  Created by zhang yinglong on 2017/8/5.
+//  Copyright © 2017年 Qiniu Cloud Storage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetDiagFramework.
+FOUNDATION_EXPORT double NetDiagFrameworkVersionNumber;
+
+//! Project version string for NetDiagFramework.
+FOUNDATION_EXPORT const unsigned char NetDiagFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetDiagFramework/PublicHeader.h>
+
+#import <QNNetDiag/QNNetDiag.h>
